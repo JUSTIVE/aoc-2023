72a07a96ddc1d1a5ebc259409769e31ada021373
Ben <ben399399@naver.com>
2023-12-05 15:19:36 +0900

    Update q2.ts

--- typescript/src/day1/q2.ts
+++ typescript/src/day1/q2.ts
@@ -2,15 +2,15 @@ import { A, S, flow } from '@mobily/ts-belt'
 import { problem, sum } from '../utilities'
 
 const stringParser = flow(
-  S.replaceAll('one', '1e'),
-  S.replaceAll('two', '2o'),
+  S.replaceAll('one', 'o1e'),
+  S.replaceAll('two', 't2o'),
   S.replaceAll('three', '3e'),
   S.replaceAll('four', '4'),
   S.replaceAll('five', '5e'),
   S.replaceAll('six', '6'),
   S.replaceAll('seven', '7n'),
-  S.replaceAll('eight', '8t'),
-  S.replaceAll('nine', '9e')
+  S.replaceAll('eight', '8'),
+  S.replaceAll('nine', '9')
 )
 
 const processLine = flow(
