1b2013d4eaa2c34ad8a11c6c799c4484b82013fb
김민상 <minsangkim@marketboro.com>
2023-12-05 13:23:37 +0900

    chore: formatted day3 q2

--- typescript/src/day3/q2.ts
+++ typescript/src/day3/q2.ts
@@ -1,16 +1,16 @@
-import { P, match } from "ts-pattern";
-import { A, B, S, flow, pipe } from "@mobily/ts-belt";
-import { getSourceLines, problem, sum } from "../utilities";
-type Coord = [number, number];
+import { P, match } from 'ts-pattern'
+import { A, B, S, flow, pipe } from '@mobily/ts-belt'
+import { problem, sum } from '../utilities'
+type Coord = [number, number]
 export type Part = {
-  value: number;
-  coord: readonly Coord[];
-};
+  value: number
+  coord: readonly Coord[]
+}
 
 export type Symbol = {
-  value: string;
-  coord: Coord;
-};
+  value: string
+  coord: Coord
+}
 
 const parseParts = (
   lineNumber: number,
@@ -20,19 +20,19 @@ const parseParts = (
   line: string
 ): Part[] =>
   match(line)
-    .with(P.union("", P.nullish), () =>
+    .with(P.union('', P.nullish), () =>
       match(numStack)
-        .with("", () => state)
+        .with('', () => state)
         .otherwise(() => [
           ...state,
           {
             value: Number(numStack),
             coord: pipe(
               numStack,
-              S.split(""),
+              S.split(''),
               A.mapWithIndex((index, _) => [lineNumber, offset - index - 1])
-            ),
-          } satisfies Part,
+            )
+          } satisfies Part
         ])
     )
     .otherwise(([head, ...tail]) =>
@@ -43,48 +43,48 @@ const parseParts = (
             offset + 1,
             numStack + head,
             state,
-            tail.join("")
+            tail.join('')
           )
         )
         .otherwise(() =>
           match(numStack)
-            .with("", () =>
-              parseParts(lineNumber, offset + 1, numStack, state, tail.join(""))
+            .with('', () =>
+              parseParts(lineNumber, offset + 1, numStack, state, tail.join(''))
             )
             .otherwise(() =>
               parseParts(
                 lineNumber,
                 offset + 1,
-                "",
+                '',
                 [
                   ...state,
                   {
                     value: Number(numStack),
                     coord: pipe(
                       numStack,
-                      S.split(""),
+                      S.split(''),
                       A.mapWithIndex((index, _) => [
                         lineNumber,
-                        offset - index - 1,
+                        offset - index - 1
                       ])
-                    ),
-                  },
+                    )
+                  }
                 ],
-                tail.join("")
+                tail.join('')
               )
             )
         )
-    );
+    )
 
 const parseSymbols = (lineNumber: number, line: string): readonly Symbol[] =>
   pipe(
     line,
-    S.split(""),
+    S.split(''),
     A.mapWithIndex((index, char) => [index, char] as const),
-    A.filter(([_, char]) => char !== "."),
+    A.filter(([_, char]) => char !== '.'),
     A.filter(([_, char]) => !/\d/.test(char)),
     A.map(([index, char]) => ({ value: char, coord: [lineNumber, index] }))
-  );
+  )
 
 const isPartAdjacentToSymbol =
   (symbol: Symbol) =>
@@ -96,38 +96,37 @@ const isPartAdjacentToSymbol =
         [x - 1, y],
         [x - 1, y + 1],
         [x, y - 1],
-        [x, y],
         [x, y + 1],
         [x + 1, y - 1],
         [x + 1, y],
-        [x + 1, y + 1],
+        [x + 1, y + 1]
       ],
       A.some(([x, y]) =>
         A.some(parts.coord, ([x1, y1]) => x === x1 && y === y1)
       )
-    );
+    )
 
 problem(3, (input) => {
   const parts = pipe(
     input,
     A.mapWithIndex((lineNumber, line) =>
-      parseParts(lineNumber, 0, "", [], line)
+      parseParts(lineNumber, 0, '', [], line)
     ),
     A.flat
-  );
+  )
 
   const symbols = pipe(
     input,
     A.mapWithIndex((lineNumber, line) => parseSymbols(lineNumber, line)),
     A.flat
-  );
+  )
 
   return pipe(
     symbols,
-    A.filter(({ value }) => value === "*"),
+    A.filter(({ value }) => value === '*'),
     A.map((symbol) => A.filter(parts, isPartAdjacentToSymbol(symbol))),
     A.filter((parts) => parts.length === 2),
     A.map(([x0, x1]) => x0.value * x1.value),
     sum
-  );
-});
+  )
+})
